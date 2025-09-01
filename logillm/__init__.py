aa1cb6a3671d52824872aaff8576359fadc93465
Michael Bommarito <michael.bommarito@gmail.com>
2025-09-01 08:42:25 -0400

    bump patch version in module

--- logillm/__init__.py
+++ logillm/__init__.py
@@ -18,10 +18,10 @@ Core Architecture:
     Core Layer (Base Abstractions)
 """
 
-__version__ = "0.2.6"
+__version__ = "0.2.7"
 
 # version tuple
-__version_info__ = (0, 2, 6)
+__version_info__ = (0, 2, 7)
 
 # Core exports
 from .core.modules import (
